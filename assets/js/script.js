5f144568340ac46ae8a8b7e6ee2d3b959b0d8da8
Ken Ilo <keneilo91@yahoo.com>
2021-08-24 14:09:32 -0500

    event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+/*buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -8,4 +15,22 @@ var createTaskHandler = function() {
     tasksToDoEl.appendChild(listItemEl); 
     }; 
   
-  buttonEl.addEventListener("click", createTaskHandler);
+  buttonEl.addEventListener("click", createTaskHandler);*/
+
+
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function(event) { 
+  
+  event.preventDefault();
+
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+ }; 
+  
+  formEl.addEventListener("submit", createTaskHandler);
